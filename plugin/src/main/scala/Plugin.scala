95f87c425ca3a15690a229d5b922ffed0558cb30
jilen <jilen.zhang@gmail.com>
2023-07-18 10:16:16 +0800

    cleanup

--- plugin/src/main/scala/Plugin.scala
+++ plugin/src/main/scala/Plugin.scala
@@ -5,9 +5,8 @@ import dotty.tools.dotc.plugins._
 class DerivePlugin extends StandardPlugin {
   val name: String = "foo"
 
-  override val description: String = "Apply derive annotation"
+  override val description: String = "Foo description"
 
   def init(options: List[String]): List[PluginPhase] =
-    val setting = Setting.parse(options)
     FooPhase() :: Nil
 }
